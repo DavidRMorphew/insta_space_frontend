a0f0626c13c42acd2d617abc867c43f07585481d
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-19 10:04:43 -0400

    add addImages action & dispatch in fetchImages

--- src/actions/imagesActions.js
+++ src/actions/imagesActions.js
@@ -1,11 +1,13 @@
 const base_url = "http://localhost:3001/images"
 
+export const addImages = (images) => ({type: 'ADD_IMAGES', payload: images})
+
 export const fetchImages = () => {
     return (dispatch) => {
         fetch(base_url)
         .then(resp => resp.json())
         .then(images => {
-            console.log(images)
+            dispatch(addImages(images))
         })
     }
 }
--- src/reducers/ImagesReducer.js
+++ src/reducers/ImagesReducer.js
@@ -1,6 +1,8 @@
 
 const ImagesReducer = (state = [], action) => {
     switch (action.type){
+        case "ADD_IMAGES":
+            return [...state, action.payload]
         default:
             return state
     }
