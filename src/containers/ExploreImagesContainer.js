496696231a9544282f010ce9b82c995a2dfe9aa8
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-19 15:45:52 -0400

    add loading ternary to ExploreImagesContainer

--- src/containers/ExploreImagesContainer.js
+++ src/containers/ExploreImagesContainer.js
@@ -1,16 +1,20 @@
 import { connect } from "react-redux"
 import { Card } from '@shopify/polaris'
 
-const ExploreImagesContainer = ({images}) => {
+const ExploreImagesContainer = ({images, loading}) => {
     
     const renderImages = () => (images.map(image => <Card key={image.title}><img src={image.image_url}></img></Card>))
 
     return(
         <div>
             <h1>Explore!</h1>
-            <ul>
-                {renderImages()}
-            </ul>
+            { loading ? 
+                <h1>Loading</h1>
+                : 
+                <ul>
+                    {renderImages()}
+                </ul>
+            }
         </div>
     )
 }
