e06d5fb78f900a4518a1b38b161b3355808cd14d
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-19 10:07:08 -0400

    add composeWithDevTools for testing

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers/index';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
