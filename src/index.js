f97c37c99e233267919d75c36ce53dd02985e247
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-19 13:07:37 -0400

    install and import polaris with AppProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '@shopify/polaris/dist/styles.css';
+// import './index.css';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import { AppProvider } from '@shopify/polaris';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
@@ -13,9 +16,11 @@ const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store} >
-    <App />
-    </Provider>
+    <AppProvider i18n={enTranslations}>
+      <Provider store={store} >
+        <App />
+      </Provider>
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
