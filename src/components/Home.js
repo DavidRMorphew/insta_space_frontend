d075ff735dd85a9a6aaa0a87a904aa7f54a23289
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-21 00:09:53 -0400

    add profile info to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
-const Home = () => {
+import { connect } from 'react-redux'
+import { Page, Card, DisplayText } from '@shopify/polaris'
+
+const Home = ({user: { username, email } }) => {
     return(
         <div>
-
+            <Page narrowWidth>
+                <br></br>
+                <Card>
+                    <br></br>
+                    <DisplayText size="Medium">Username</DisplayText>
+                    <DisplayText size="Small">{username}</DisplayText>
+                        <br></br>
+                    <DisplayText size="Medium">Email</DisplayText>
+                    <DisplayText size="Small">{email}</DisplayText>
+                        <br></br>
+                
+                </Card>
+            </Page>
         </div>
     )
 }
 
-export default Home
+export default connect(({ user }) => ({ user }))(Home)
