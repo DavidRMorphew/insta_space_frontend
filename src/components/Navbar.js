6e2465a4b656b312f89691e00d7358fe8d07d6d9
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-20 22:58:28 -0400

    add styling to Navbar Navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,24 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 
+    const active = {
+        background: "black",
+        color: "lightgray"
+    }
+
     return(
         <div>
             <NavLink
                 to="/explore"
                 className="navlink"
+                activeStyle={active}
             >
                 Explore
             </NavLink>
             <NavLink
                 to="/home"
                 className="navlink"
+                activeStyle={active}
             >
                 Home
             </NavLink>
