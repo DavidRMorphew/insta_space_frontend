5164ded355f4f970ecedd0e4afc1f7f1d24b4dda
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-09-26 21:34:51 -0400

    remove extraneous files and code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@shopify/polaris/dist/styles.css';
-// import './index.css';
 import enTranslations from '@shopify/polaris/locales/en.json';
 import { AppProvider } from '@shopify/polaris';
 import App from './App';
@@ -25,7 +24,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
